f45935047f8d2c46a4dd5433f97230160c30c5d3
lsfernandes92 <lsfernandes92@gmail.com>
2019-09-10 16:54:50 -0700

    Add better hotel class

--- hotel.js
+++ hotel.js
@@ -1,57 +1,31 @@
 class Hotel {
-  // construtor cheio de parametros AAAAAAAAAAA
-  constructor(name, rank, weekPriceRegularClient, weekendPriceRegularClient,
-    weekPriceRewardClient, weekendPriceRewardClient) {
+  constructor(name, rank, options) {
     this._name = name;
     this._rank = rank;
-    this._weekPriceRegularClient = weekPriceRegularClient;
-    this._weekendPriceRegularClient = weekendPriceRegularClient;
-    this._weekPriceRewardClient = weekPriceRewardClient;
-    this._weekendPriceRewardClient = weekendPriceRewardClient;
+    this._weekPriceRegularClient = options.weekPriceRegularClient;
+    this._weekendPriceRegularClient = options.weekendPriceRegularClient;
+    this._weekPriceRewardClient = options.weekPriceRewardClient;
+    this._weekendPriceRewardClient = options.weekendPriceRewardClient;
   }
 
-  // modelo anemico :S
   get name() { return this._name; }
   get rank() { return this._rank; }
   get weekPriceRegularClient() { return  this._weekPriceRegularClient; }
   get weekendPriceRegularClient() { return  this._weekendPriceRegularClient; }
-  get weekPriceRewardClient() { return this._weekPriceRegularClient; }
-  get weekendPriceRewardClient() { return this._weekeendPriceRegularClient; }
+  get weekPriceRewardClient() { return this._weekPriceRewardClient; }
+  get weekendPriceRewardClient() { return this._weekendPriceRewardClient; }
 
-  set name(newName) { this._name = newName; }
-  set rank(newRank) { this._rank = newRank; }
-  set weekPriceRegularClient(newPrice) { this._weekPriceRegularClient = newPrice; }
-  set weekeendPriceRegularClient(newPrice) { this._weekeendPriceRegularClient = newPrice; }
-  set weekPriceRewardClient(newPrice) { this._weekPriceRewardClient = newPrice; }
-  set weekeendPriceRewardClient(newPrice) { this._weekeendPriceRewardClient = newPrice; }
-
-  getPriceFromDates(clientType, ...dates) {
-    let totalPrice = this._getPrice(clientType, dates[0]);
-
-    dates.shift();
-
-    dates.forEach(date => {
-      let priceFromDate = this._getPrice(clientType, date);
-      totalPrice += priceFromDate;
-    });
-
-    return totalPrice;
-  }
-
-  _getPrice(clientType, date) {
-    let day = new Date(date).getDay();
-
-    let weekend = (day === 6) || (day === 0);
-
-    if (clientType.toLowerCase() === 'regular') {
-      return (
-        weekend ? this._weekendPriceRegularClient : this._weekPriceRegularClient
-      );
-    } else {
-      return (
-        weekend ? this._weekendPriceRewardClient : this._weekPriceRewardClient
-      );
-    }
+  toString() {
+    return(
+      `
+        Hotel: ${this.name}, classificação: ${this.rank}
+        Preços:
+        \tSemana cliente normal: ${this.weekPriceRegularClient}
+        \tFDS cliente normal: ${this.weekendPriceRegularClient}
+        \tFDS cliente fidelidade: ${this.weekendPriceRewardClient}
+        \tSemana cliente fidelidade: ${this.weekPriceRewardClient}
+      `
+    )
   }
 }
 
